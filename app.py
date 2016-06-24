b8e29acad203a0a8a391a640ba1876ffb9c56c2a
Ernest Wong <chewong@uwaterloo.ca>
2016-06-24 15:41:19 -0400

    Code Beautified

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		print request.form.get('first')
-		print request.form.get('second')
-	return render_template('index.html')
-	
+    if request.method == 'POST':
+        print request.form.get('first')
+        print request.form.get('second')
+    return render_template('index.html')
+
 if __name__ == '__main__':
-   app.run(port=8000)
+    app.run(port=8000)
+    
