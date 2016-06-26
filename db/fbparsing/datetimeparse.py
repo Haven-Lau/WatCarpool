e8d05686d8067dc1af80975a9268761fb8f3eab1
Haven Lau <havenlau@hotmail.com>
2016-06-26 03:03:06 -0400

    Added range
    Still need phone number prevention

--- db/fbparsing/datetimeparse.py
+++ db/fbparsing/datetimeparse.py
@@ -26,68 +26,68 @@ while True:
         
         # Sanitize 'ish' that people sometimes like to use
         raw_message = entry1["message"].lower().replace("ish", "")
+        if 'driving' in raw_message: 
+	        #places = GeoText(titled_message)
+	        #url = 'http://www.bbc.com/news/world-europe-26919928'
+	        #places = geograpy.get_place_context(text=entry1['message'])
 
-        #places = GeoText(titled_message)
-        #url = 'http://www.bbc.com/news/world-europe-26919928'
-        #places = geograpy.get_place_context(text=entry1['message'])
+	        time_string = entry1["updated_time"]
+	        time_string = time_string[:10]+' '+time_string[11:-5]
+	        message_time = cal.parseDT(time_string, now)[0]
+	        message_time = message_time - timedelta(days=1)
+	        print "message time: "+ str(message_time)
+	        if now - timedelta(days=1) - timedelta(hours=12) > message_time:
+	            breaking = True
+	            break
+	        #print("Post date:\t%s" % message_time)
+	        # Recognition precedence: Weekday == Relation > Date Time
+	        # Weekday scan
+	        for weekday in weekdays:
+	            if weekday in raw_message:
+	                raw_message = raw_message.replace(weekday,'')
+	                weekday_date = cal.parseDT(weekday, message_time)[0]
+	                message_date = weekday_date.date()
+	                #print ("Weekday found:\t%s\t%s" % (weekday,weekday_date))
+	                break
 
-        time_string = entry1["updated_time"]
-        time_string = time_string[:10]+' '+time_string[11:-5]
-        message_time = cal.parseDT(time_string, now)[0]
-        message_time = message_time - timedelta(hours=5)
-        print "message time: "+ str(message_time)
-        if now - timedelta(days=1) > message_time:
-            breaking = True
-            break
-        #print("Post date:\t%s" % message_time)
-        # Recognition precedence: Weekday == Relation > Date Time
-        # Weekday scan
-        for weekday in weekdays:
-            if weekday in raw_message:
-                raw_message = raw_message.replace(weekday,'')
-                weekday_date = cal.parseDT(weekday, message_time)[0]
-                message_date = weekday_date.date()
-                #print ("Weekday found:\t%s\t%s" % (weekday,weekday_date))
-                break
+	        # Relation scan
+	        for nextday in nextdays:
+	            if nextday in raw_message:
+	                raw_message = raw_message.replace(nextday,'')
+	                nextday_date = cal.parseDT(nextday, message_time)[0]
+	                message_date = nextday_date.date()
+	                #print ("Relation found:\t%s\t%s" % (nextday,nextday_date))
+	                break
 
-        # Relation scan
-        for nextday in nextdays:
-            if nextday in raw_message:
-                raw_message = raw_message.replace(nextday,'')
-                nextday_date = cal.parseDT(nextday, message_time)[0]
-                message_date = nextday_date.date()
-                #print ("Relation found:\t%s\t%s" % (nextday,nextday_date))
-                break
+	        # Check if day matches relation
+	        if (nextday_date is not None and weekday_date is not None):
+	            if (nextday_date.date() == weekday_date.date()):
+	                message_date = nextday_date.date()
+	                print ("Relation and Weekday matches")
+	            else:
+	                print ("Relation and Weekday DO NOT MATCH!!")
 
-        # Check if day matches relation
-        if (nextday_date is not None and weekday_date is not None):
-            if (nextday_date.date() == weekday_date.date()):
-                message_date = nextday_date.date()
-                print ("Relation and Weekday matches")
-            else:
-                print ("Relation and Weekday DO NOT MATCH!!")
+	        # Remaining message datetime scan
+	        final_datetime = cal.parseDT(raw_message, message_time)[0]
+	        #print("Remaining msg:\t%s\t%s" % (raw_message,final_datetime))
 
-        # Remaining message datetime scan
-        final_datetime = cal.parseDT(raw_message, message_time)[0]
-        #print("Remaining msg:\t%s\t%s" % (raw_message,final_datetime))
+	        if (message_date is not None):
+	            final_datetime = final_datetime.replace(year=message_date.year,month=message_date.month,day=message_date.day)
 
-        if (message_date is not None):
-            final_datetime = final_datetime.replace(year=message_date.year,month=message_date.month,day=message_date.day)
+	        # Location Scan
+	        location_dict = loc_scan.location_scan(entry1["message"])
 
-        # Location Scan
-        location_dict = loc_scan.location_scan(entry1["message"])
-
-        # Price 
-        for entry in entry1["message"].split():
-            if '$' in entry:
-                price = entry
+	        # Price 
+	        for entry in entry1["message"].split():
+	            if '$' in entry:
+	                price = entry
 
 
-        # Outputs
-        print ("Final date: %s" % (final_datetime))
-        print ("From: %s" % (location_dict['From']))
-        print ("To: %s" % (location_dict['To']))
-        print ("Price: %s" % price)
+	        # Outputs
+	        print ("Final date: %s" % (final_datetime))
+	        print ("From: %s" % (location_dict['From']))
+	        print ("To: %s" % (location_dict['To']))
+	        print ("Price: %s" % price)
     link = response.json()['paging']['next']
     if breaking:
         break
